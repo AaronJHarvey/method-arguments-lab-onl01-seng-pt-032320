cdd734bc5ff90c91940e0ae6744a9f7da9c7c6d5
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-24 22:21:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,8 @@ def introduction(name)
 end
 introduction("Aaron")
 
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+def introduction_with_language(name, language)
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_language("Dan","Ember.js")
+introduction_with_language("Dan", "Ember.js")
   
